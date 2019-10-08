c41352e515cb80fd28757b3e80e5912e87ecfd88
Kevin Brewster <brewsterdesigns@gmail.com>
2019-10-08 14:46:58 -0400

    made public

--- Sources/SwiftLinuxGATTServerExampleAdvanced/main.swift
+++ Sources/SwiftLinuxGATTServerExampleAdvanced/main.swift
@@ -4,14 +4,8 @@ import BluetoothLinux
 
 if let hostController = HostController.default {
     do {
-        
-        //let x = TemperatureService(peripheral: TestPeripheral())
-        //print("x characteristics = \(x.characteristics)")
-       
-        _ = try FermentPeripheral(hostController: hostController)
+        _ = try ThermometerPeripheral(hostController: hostController)
         while true { }
-        
-        
     } catch let error {
         print("Error initializing peripheral: \(error)")
     }
